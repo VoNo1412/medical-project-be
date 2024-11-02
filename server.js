dea906b96036cdf070ea5e8a7a11cf8e00dbcacb
TuPM <tupm.citgroup@gmail.com>
2024-11-02 19:55:23 +0700

    add

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://545b-42-114-32-182.ngrok-free.app'],
+    origin: [
+        'http://localhost:3000',
+        'https://545b-42-114-32-182.ngrok-free.app',
+        'https://nhakhoa-72d284770bf0.herokuapp.com'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
