02539f73e1e7f0f460fae76b8501a45788f53a4c
TuPM <tupm.citgroup@gmail.com>
2024-11-04 00:12:42 +0700

    add

--- server.js
+++ server.js
@@ -40,13 +40,16 @@ app.use(cors({
 let db = null;
 
 async function connectDatabase() {
-    db = await mysql.createConnection({
+    db = await mysql.createPool({
         host: process.env.DB_HOST || "103.221.222.62",
         user: process.env.DB_USER || "waghgljj_nhakhoauser",
         port: process.env.DB_PORT || 3306,
         password: process.env.DB_PASSWORD || "!pr9bTW8vmeLwHY",
         database: process.env.DB_NAME || "waghgljj_nhakhoa",
-        timezone: '+07:00'
+        timezone: '+07:00',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
     });
     console.log("Database connected");
 }
