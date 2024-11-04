9acf36f90d28959b27e248f7fa7c9bb0986820e9
TuPM <tupm.citgroup@gmail.com>
2024-11-04 08:57:25 +0700

    add

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -4,7 +4,7 @@ const appointmentController = require('../controllers/appointmentController');
 const router = express.Router();
 
 router.post('/book-appointment', appointmentController.bookAppointment);
-router.get('/appointments', appointmentController.getAppointments);
+router.get('/', appointmentController.getAppointments);
 router.put('/hour_appointments/:id', appointmentController.updateAppointment);
 router.delete('/hour_appointments/:id', appointmentController.deleteAppointment);
 
