01ba31587641f03ccb73eebf4bd9627f78c94ead
TuPM <tupm.citgroup@gmail.com>
2024-11-04 08:55:48 +0700

    add

---
+++ db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST || "103.221.222.62",
+    user: process.env.DB_USER || "waghgljj_nhakhoauser",
+    port: process.env.DB_PORT || 3306,
+    password: process.env.DB_PASSWORD || "!pr9bTW8vmeLwHY",
+    database: process.env.DB_NAME || "waghgljj_nhakhoa",
+    timezone: '+07:00',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
